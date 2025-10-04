90beac28f8789cf9b61b69dc455b17738eedece4
Daniel Carriba <dcarriba@gmx.net>
2025-10-04 14:40:14 +0200

    New Gradle project

---
+++ src/main/java/com/dcarriba/Main.java
@@ -0,0 +1,14 @@
+package com.dcarriba;
+
+/**
+ * Main class of the project
+ */
+public class Main {
+
+    /**
+     * Main function of the project
+     */
+    public static void main(String[] args) {
+
+    }
+}
