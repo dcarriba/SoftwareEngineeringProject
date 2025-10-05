416d65ae6012ff2c01c89404080d17495304b342
Daniel Carriba <dcarriba@gmx.net>
2025-10-05 17:51:08 +0200

    Added BitPackingTest unit test class

---
+++ src/test/java/com/dcarriba/bitpacking/BitPackingTest.java
@@ -0,0 +1,62 @@
+package com.dcarriba.bitpacking;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * {@link BitPackingTest} tests the already implemented methods of the abstract {@link BitPacking} class
+ */
+public class BitPackingTest {
+
+    /**
+     * Subclass needed to instantiate the abstract {@link BitPacking} class
+     */
+    private static class BitPackingSub extends BitPacking {
+        @Override
+        public void compress(int[] array) {
+        }
+
+        @Override
+        public void decompress(int[] array) {
+        }
+
+        @Override
+        public int get(int i) {
+            return 0;
+        }
+    }
+
+    @Test
+    void testCalculateBitSizeWithOnlyZeros() {
+        BitPackingSub bitPackingSub = new BitPackingSub();
+        int[] array = {0, 0, 0};
+        assertEquals(1, bitPackingSub.calculateBitSize(array));
+    }
+
+    @Test
+    void testCalculateBitSizeWithSmallValue() {
+        BitPackingSub bitPackingSub = new BitPackingSub();
+        int[] array = {1, 2, 3, 7};
+        assertEquals(3, bitPackingSub.calculateBitSize(array));
+    }
+
+    @Test
+    void testCalculateBitSizeWithLargeValue() {
+        BitPackingSub bitPackingSub = new BitPackingSub();
+        int[] array = {0, 1023};
+        assertEquals(10, bitPackingSub.calculateBitSize(array));
+    }
+
+    @Test
+    void testCalculateBitSizeThrowsOnNegativeValue() {
+        BitPackingSub bitPackingSub = new BitPackingSub();
+        int[] array = {5, -3, 8};
+        assertThrows(IllegalArgumentException.class, () -> bitPackingSub.calculateBitSize(array));
+    }
+
+    @Test
+    void testCalculateBitSizeThrowsOnNullArray() {
+        BitPackingSub bitPackingSub = new BitPackingSub();
+        assertThrows(IllegalArgumentException.class, () -> bitPackingSub.calculateBitSize(null));
+    }
+}
