f7a6f44ce820941f67c9cf3bc476da4fef948313
Daniel Carriba <dcarriba@gmx.net>
2025-10-29 13:03:54 +0100

    javadoc BitPackingWithOverflowAreaTest

--- src/test/java/com/dcarriba/bitpacking/versions/BitPackingWithOverflowAreaTest.java
+++ src/test/java/com/dcarriba/bitpacking/versions/BitPackingWithOverflowAreaTest.java
@@ -4,6 +4,10 @@ import com.dcarriba.bitpacking.BitPacking;
 import com.dcarriba.bitpacking.factory.BitPackingFactory;
 import com.dcarriba.bitpacking.factory.CompressionVersion;
 
+/**
+ * {@link BitPackingWithOverflowAreaTest} provides unit tests for {@link BitPackingWithOverflowArea}
+ * and extends {@link BitPackingVersionsBaseTest} for the test logic
+ */
 class BitPackingWithOverflowAreaTest extends BitPackingVersionsBaseTest {
 
     @Override
